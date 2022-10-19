b8cc5ed4723d2b5dd69875827af5f3f1f12d216b
Evan McLean <evanhughmclean@gmail.com>
2022-10-19 13:43:15 -0400

    feat: Add static personal detail component

--- src/components/Form/FormComponents/PersonalDetails.js
+++ src/components/Form/FormComponents/PersonalDetails.js
@@ -2,14 +2,48 @@ import React, { Component } from "react";
 
 class PersonalDetails extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
-    return(
-      <div></div>
-    )
+    return (
+      <div className="form-section">
+        <h2>Personal Details</h2>
+        <div className="form-element">
+          <label htmlFor="name">Full Name</label>
+          <input type="text" id="name" placeholder="Enter Full Name" />
+        </div>
+        <div className="form-element">
+          <label htmlFor="title">Job Title</label>
+          <input type="text" id="title" placeholder="Enter Job Title" />
+        </div>
+        <div className="form-element">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="Enter Email" />
+        </div>
+        <div className="form-element">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            type="text"
+            id="phoneNumber"
+            placeholder="Enter Phone Number"
+          />
+        </div>
+        <div className="form-element">
+          <label htmlFor="location">Location</label>
+          <input type="text" id="location" placeholder="Enter Location" />
+        </div>
+        <div className="form-element">
+          <label htmlFor="photo">Upload a photo</label>
+          <input type="file" id="photo" />
+        </div>
+        <div className="form-element">
+          <label htmlFor="summary">Summary</label>
+          <textarea id="summary" placeholder="Introduce yourself"></textarea>
+        </div>
+      </div>
+    );
   }
 }
 
-export default PersonalDetails
+export default PersonalDetails;
