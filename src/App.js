4066b65b35cdfede67a825799ae1412c21d33c07
Evan McLean <evanhughmclean@gmail.com>
2022-11-07 10:39:42 -0500

    feat: change App to function component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
-
-export default App;
